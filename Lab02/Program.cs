9558dcd26ae7988ef8268c41ac4e1fe7d6081ae0
athichangam-bit <athicha.ngam@bumail.net>
2026-08-29 03:01:44 +0700

    Add lab02

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -12,13 +12,13 @@ namespace Lab02
     {
         static void Main(string[] args)
         {
-            string bossName = "Uia";
+            string bossName = "Pawdy";
             char rank = 'F';
             int level = 1;
             int maxHp = 70;
-            int currentHp = 27;
-            float attackPower = 17.5f;
-            double critMultiplier = 1.5;
+            int currentHp = 43;
+            float attackPower = 12.5f;
+            double critMultiplier = 0.75;
             bool isBoss = true;
 
             Console.WriteLine("======= BOSS STATUS: INITIAL =======");
@@ -34,6 +34,29 @@ namespace Lab02
             Console.WriteLine("======= BOSS STATUS: AFTER DAMADE =======");
             Console.WriteLine($"HP: {currentHp} / {maxHp}%");
             Console.WriteLine($"HP Percent: 37%");
+
+            string bossName = "Drabby";
+            char rank = 'C';
+            int level = 3;
+            int maxHp = 180;
+            int currentHp = 67;
+            float attackPower = 27.5f;
+            double critMultiplier = 1.25;
+            bool isBoss = true;
+
+            Console.WriteLine("======= BOSS STATUS: INITIAL =======");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}%");
+            int hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"HP: {hpPercent}%");
+            Console.WriteLine();
+            Console.WriteLine("Uai takes 15 damage!");
+            Console.WriteLine();
+            Console.WriteLine("======= BOSS STATUS: AFTER DAMADE =======");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}%");
+            Console.WriteLine($"HP Percent: 48%");
         }
     }
 }
