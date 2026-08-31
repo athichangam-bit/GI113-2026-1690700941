b0386367185f8a80a7a95f51bcd960a47293ebfd
athichangam-bit <athicha.ngam@bumail.net>
2026-08-31 11:43:14 +0700

    Add lab02

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -14,49 +14,57 @@ namespace Lab02
         {
             string bossName = "Pawdy";
             char rank = 'F';
-            int level = 1;
+            int level = 3;
             int maxHp = 70;
             int currentHp = 43;
             float attackPower = 12.5f;
             double critMultiplier = 0.75;
             bool isBoss = true;
 
-            Console.WriteLine("======= BOSS STATUS: INITIAL =======");
+            Console.WriteLine("======= BOSS STATUS: INITIAL =======\n");
             Console.WriteLine($"Name: {bossName}");
             Console.WriteLine($"Rank: {rank}");
             Console.WriteLine($"Level: {level}");
-            Console.WriteLine($"HP: {currentHp} / {maxHp}%");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
             int hpPercent = currentHp * 100 / maxHp;
-            Console.WriteLine($"HP: {hpPercent}%");
+            Console.WriteLine($"HP Percent: {hpPercent}%");
             Console.WriteLine();
-            Console.WriteLine("Uai takes 15 damage!");
+            int takesDamage = 15;   //โดนดาเมจ
+            Console.WriteLine("== !Pawdy takes 15 damage! ==");
             Console.WriteLine();
-            Console.WriteLine("======= BOSS STATUS: AFTER DAMADE =======");
-            Console.WriteLine($"HP: {currentHp} / {maxHp}%");
-            Console.WriteLine($"HP Percent: 37%");
+            Console.WriteLine("======= BOSS STATUS: AFTER DAMADE =======\n");
+            int afterHp = currentHp - takesDamage;   //เลือดลด
+            Console.WriteLine($"HP: {afterHp} / {maxHp}");
+            int afterHpPercent = afterHp * 100 / maxHp;   //Percent หลังจากโดน Damage
+            Console.WriteLine($"HP Percent: {afterHpPercent}%");
+            Console.WriteLine("\n----------------------------------------------\n\n");
 
-            string bossName = "Drabby";
-            char rank = 'C';
-            int level = 3;
-            int maxHp = 180;
-            int currentHp = 67;
-            float attackPower = 27.5f;
-            double critMultiplier = 1.25;
-            bool isBoss = true;
+            string bossName01 = "Drabby";
+            char rank01 = 'C';
+            int level01 = 7;
+            int maxHp01 = 130;
+            int currentHp01 = 76;
+            float attackPower01 = 17.5f;
+            double critMultiplier01 = 1.25;
+            bool isBoss01 = true;
 
-            Console.WriteLine("======= BOSS STATUS: INITIAL =======");
-            Console.WriteLine($"Name: {bossName}");
-            Console.WriteLine($"Rank: {rank}");
-            Console.WriteLine($"Level: {level}");
-            Console.WriteLine($"HP: {currentHp} / {maxHp}%");
-            int hpPercent = currentHp * 100 / maxHp;
-            Console.WriteLine($"HP: {hpPercent}%");
+            Console.WriteLine("======= BOSS STATUS: INITIAL =======\n");
+            Console.WriteLine($"Name: {bossName01}");
+            Console.WriteLine($"Rank: {rank01}");
+            Console.WriteLine($"Level: {level01}");
+            Console.WriteLine($"HP: {currentHp01} / {maxHp01}");
+            int hpPercent01 = currentHp01 * 100 / maxHp01;          
+            Console.WriteLine($"HP Percent: {hpPercent01}%");
             Console.WriteLine();
-            Console.WriteLine("Uai takes 15 damage!");
+            int takesDamage01 = 20;   //โดนดาเมจ
+            Console.WriteLine("== !Drabby takes 20 damage! ==");
             Console.WriteLine();
-            Console.WriteLine("======= BOSS STATUS: AFTER DAMADE =======");
-            Console.WriteLine($"HP: {currentHp} / {maxHp}%");
-            Console.WriteLine($"HP Percent: 48%");
+            Console.WriteLine("======= BOSS STATUS: AFTER DAMADE =======\n");
+            int afterHp01 = currentHp01 - takesDamage01;   //เลือดลด
+            Console.WriteLine($"HP: {afterHp01} / {maxHp01}");
+            int afterHpPercent01 = afterHp01 * 100 / maxHp01;   //Percent หลังจากโดน Damage
+            Console.WriteLine($"HP Percent: {afterHpPercent01}%");
+            Console.WriteLine("\n----------------------------------------------\n\n");
         }
     }
 }
